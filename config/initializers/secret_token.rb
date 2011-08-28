c112907d392dd6430bd1c5c94ee4af6822c5fa74
Michael Mottola <mikemottola@gmail.com>
2011-08-28 14:33:42 -0400

    Initial Import of all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VendettaMobile::Application.config.secret_token = '4d26e65a8243cad171badbcb6623096db1dd7271cb841c23fd900adf05fec82f2ffce57e1f66ea7370cb7748ceba9f1357e71514aa2773863bba6399a0895442'

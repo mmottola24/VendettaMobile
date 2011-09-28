1334d52fc2866cc4937fea9f2725932d0436e5aa
Michael Mottola <mikemottola@gmail.com>
2011-09-27 20:09:24 -0400

    added results to the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,9 @@ class HomeController < ApplicationController
 
     @games = (get_games reg_dates, data['regular']) + (get_games playoff_dates, data['playoffs'])
 
+    #Get latest around the league
+    @results = get_hockey_data 'results'
+
     render 'pages/index'
 
   end

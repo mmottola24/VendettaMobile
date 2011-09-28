59883f3c60da40dabdbc88758f0336bc7331a21d
Michael Mottola <mikemottola@gmail.com>
2011-09-27 20:43:39 -0400

    added results page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,15 @@
 class ResultsController < ApplicationController
 
+  def index
+    @title = "Scores"
+    @page = "results"
 
+    @results = get_hockey_data "results"
+
+    respond_to do |format|
+      format.html { render 'pages/results' }
+      format.json { render :json => @results }
+    end
+  end
 
 end
